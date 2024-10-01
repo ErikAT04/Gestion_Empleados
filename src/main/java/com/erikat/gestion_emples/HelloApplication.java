36af220eb4c901317ac87a3127de3a4122df59e9
DAM <aterik.dev@gmail.com>
2024-10-01 13:29:42 +0200

    Conexión correcta a FXML

--- src/main/java/com/erikat/gestion_emples/HelloApplication.java
+++ src/main/java/com/erikat/gestion_emples/HelloApplication.java
@@ -1,6 +1,7 @@
 package com.erikat.gestion_emples;
 
 import com.erikat.gestion_emples.Utils.DatabaseManager;
+import com.erikat.gestion_emples.Utils.R;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
@@ -11,7 +12,8 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException, ClassNotFoundException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader();
+        fxmlLoader.setLocation(R.getFXML("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
         stage.setTitle("Hello!");
         stage.setScene(scene);
--- src/main/java/com/erikat/gestion_emples/Utils/R.java
+++ src/main/java/com/erikat/gestion_emples/Utils/R.java
@@ -2,10 +2,15 @@ package com.erikat.gestion_emples.Utils;
 
 import java.io.File;
 import java.io.InputStream;
+import java.net.URL;
 
 public class R {
 
     public static InputStream getProperties(String name) {
         return Thread.currentThread().getContextClassLoader().getResourceAsStream("config" + File.separator + name);
     }
+    public static URL getFXML(String name){
+        return Thread.currentThread().getContextClassLoader().getResource("ui" + File.separator + name);
+    }
+
 }

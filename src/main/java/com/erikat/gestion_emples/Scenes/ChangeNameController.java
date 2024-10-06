bfd06739294022554bf5c50b5fb3ff39cba9fa0b
Erik <aterik.dev@gmail.com>
2024-10-06 13:05:53 +0200

    Pequeño cambio 2

--- src/main/java/com/erikat/gestion_emples/Scenes/ChangeNameController.java
+++ src/main/java/com/erikat/gestion_emples/Scenes/ChangeNameController.java
@@ -18,8 +18,10 @@ import org.apache.commons.codec.digest.DigestUtils;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+//CONTROLADOR QUE AFECTA AL FICHERO 'changeName.fxml'
+
 public class ChangeNameController extends Controller implements Initializable {
-    EnterpriseDAO enterDAO;
+    EnterpriseDAO enterDAO; //Necesita acceder a la clase DAO de Empresas
     @FXML
     private Button changeBtt;
 
@@ -31,23 +33,30 @@ public class ChangeNameController extends Controller implements Initializable {
 
     @FXML
     void onChangeClick(ActionEvent event) {
-        if(nameTField.getText().isEmpty() || passField.getText().isEmpty()) {
+        /*
+        Tiene que cumplir ciertas condiciones:
+            1-Los campos no pueden estar vacíos
+            2-El nombre de usuario no puede ser el mismo que el que intentamos poner
+            3-La contraseña tiene que ser correcta para hacer el cambio
+            4-El nombre de usuario no puede repetirse en la base de datos (Unique)
+         */
+        if (nameTField.getText().isEmpty() || passField.getText().isEmpty()) { //Si hay campos sin rellenar
             AlertUtil.showAlert("Error de introducción", "Alguno de los campos está vacío", Alert.AlertType.ERROR);
         } else {
             String pass = DigestUtils.sha256Hex(passField.getText());
-            if (nameTField.getText().equals(DatabaseManager.enterp.getEnter_name())){
+            if (nameTField.getText().equals(DatabaseManager.enterp.getEnter_name())) { //Si el nombre coincide con el anterior
                 AlertUtil.showAlert("Error de nombre", "Ya estás usando este nombre", Alert.AlertType.ERROR);
             } else {
-                if (!pass.equals(DatabaseManager.enterp.getEnter_passwd())){
+                if (!pass.equals(DatabaseManager.enterp.getEnter_passwd())) { //Si la contraseña encrpitada no es igual que la actual
                     AlertUtil.showAlert("Error de contraseña", "Las contraseñas no coinciden en la base de datos", Alert.AlertType.ERROR);
                 } else {
                     Enterprise enter = new Enterprise(DatabaseManager.enterp.getId(), nameTField.getText(), DatabaseManager.enterp.getEnter_passwd());
                     //Antes de cambiar datos en la aplicación, uso una copia del objeto. Es necesario crear un nuevo objeto porque, de lo contrario, modificaría la dirección de memoria
                     //Además, en el atributo del nombre pongo el nombre nuevo
-                    if (enterDAO.updateEnterprise(enter)==1){
+                    if (enterDAO.updateEnterprise(enter) == 1) { //Si el update sale correctamente:
                         AlertUtil.showAlert("Cambio de nombre", "Nombre de cuenta cambiada correctamente", Alert.AlertType.INFORMATION);
                         DatabaseManager.enterp = enter; //Ahora sí, se iguala al nuevo usuario, ya que se ha cambiado sin problema
-                        ((Stage)this.changeBtt.getScene().getWindow()).close();
+                        ((Stage) this.changeBtt.getScene().getWindow()).close();
                     } else {
                         AlertUtil.showAlert("Error de cuenta", "Ya hay un usuario con ese nombre de cuenta", Alert.AlertType.ERROR);
                     }
@@ -57,7 +66,7 @@ public class ChangeNameController extends Controller implements Initializable {
     }
 
     @Override
-    public void initialize(URL url, ResourceBundle resourceBundle) {
+    public void initialize(URL url, ResourceBundle resourceBundle) { //Inicializa el controlador para iniciar el objeto DAO
         enterDAO = new EnterpriseDAO();
     }
 }
